8e2e05ffb545ff56a9b1268feb71aa5a1e50917c
zhengyitian <37394193+zhengyitian@users.noreply.github.com>
2019-06-17 23:34:56 +0800

    Add files via upload

--- statusServer.py
+++ statusServer.py
@@ -4,7 +4,7 @@ import select,socket,copy
 from helpFunc import *
 
 class statusServer():
-    def __init__(self,port,toleranceTime,salt):     
+    def __init__(self,port,toleranceTime,salt,isLocalTest=False):     
         self.lastTime = 0        
         self.ip = '0.0.0.0'
         self.port = port
@@ -15,6 +15,13 @@ class statusServer():
         self.toleranceTime = toleranceTime
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind(('0.0.0.0',self.port))  
+        self.isLocalTest = isLocalTest
+    def isTest(self):
+        if not self.isLocalTest:
+            return False
+        r = random.randint(1,10)
+        if r < 3:
+            return True
         
     @gen.coroutine
     def getClientStatus(self):
@@ -28,23 +35,29 @@ class statusServer():
             yield gen.sleep(miniSleep)
         self.serverStatus = m    
         
+    @gen.coroutine
     def doWork(self):
-        r = select.select([sock],[],[],0)
-        if not r[0]:
-            return 
-        data, addr = sock.recvfrom(recLen)
-        uuid ,ss = checkPackValid_server(data,self.salt)
-        if not uuid:
-            return
-        m = json.loads(ss)
-        ti = m['time']
-        if ti>lastTime:
-            self.lastTime = ti
-            self.clientStatus = m   
-            self.lastUpdateTime = time.time()
-        m = copy.deepcopy(self.serverStatus)
-        m['time']=time.time()
-        j = json.dumps(m)
-        data = makePack_server(j, uuid, self.salt)
-        sock.sendto(data,addr)
+        while True:
+            r = select.select([self.sock],[],[],0)
+            if not r[0]:
+                return 
+            data, addr = self.sock.recvfrom(recLen)
+            uuid ,ss = checkPackValid_server(data,self.salt)
+            if not uuid or self.isTest():
+                continue
+            m = json.loads(ss)
+            ti = m['time']
+            if ti>lastTime:
+                self.lastTime = ti
+                self.clientStatus = m   
+                self.lastUpdateTime = time.time()
+            m = copy.deepcopy(self.serverStatus)
+            m['time']=time.time()
+            j = json.dumps(m)
+            data = makePack_server(j, uuid, self.salt)
+            if self.isTest():
+                continue
+            if self.isLocalTest:
+                yield gen.sleep(random.randint(200,300)/1000.0)
+                self.sock.sendto(data,addr)
 
